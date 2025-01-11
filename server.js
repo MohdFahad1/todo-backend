c0135368286a53c4f6f8c086871aeb8b20d06a78
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2025-01-11 22:55:45 +0530

    feat: Added authRoutes

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectToMongo = require("./src/db/db");
+const authRoutes = require("./src/routes/authRoutes");
 
 dotenv.config();
 
 connectToMongo();
+
 const PORT = process.env.PORT || 5000;
+
 const app = express();
+
+//Middlewares
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
