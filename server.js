2c4b92a5418feefd5bdda06e9221833dd849f4ab
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2025-01-13 23:23:39 +0530

    fix: cors origin

--- server.js
+++ server.js
@@ -16,13 +16,24 @@ const app = express();
 
 const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
 
+const allowedOrigins = ["https://todo-frontend-three-rho.vercel.app/"];
+
 //Middlewares
 app.use(
   cors({
-    origin: CLIENT_URL,
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
   })
 );
+
 app.use(express.json());
 
 // Routes
